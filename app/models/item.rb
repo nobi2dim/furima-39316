1ce8bf8a8166e27e4c1d1b52845169f172a86496
nobi2dim <nobi2dim@gmail.com>
2023-05-11 12:31:20 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :sender
+  belongs_to :shipment
 end
