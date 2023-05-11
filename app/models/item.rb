11d1e13b77343d6d0a5767de528cf86eff4e8d01
nobi2dim <nobi2dim@gmail.com>
2023-05-11 16:23:30 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :detail, presence: true
-  validates :price, format: { with: /\A[0-9]+\z/ },
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :detail, presence: true, length: { maximum: 1000 }
+  validates :price, format: { with: /\A[0-9]+\z/ }, presence: true,
             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    name         { 'Test' }
+    detail       { 'テスト用の商品説明' }
+    postage_id   { 2 }
+    price        { 1000 }
+    shipment_id  { 2 }
+    category_id  { 2 }
+    sender_id    { 2 }
+    condition_id { 2 }
+    association :user
   end
 end
