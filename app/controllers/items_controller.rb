eae418330d4321dc5a2d34424132d190e94fd3a2
nobi2dim <nobi2dim@gmail.com>
2023-05-12 19:48:54 +0900

    商品削除機能の修正(1)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
+    return unless item.user.id == current_user.id
+
     item.destroy
     redirect_to root_path
   end
