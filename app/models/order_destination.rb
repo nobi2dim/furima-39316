86126817615ce981ce774320b29abfe5a5edd09f
nobi2dim <nobi2dim@gmail.com>
2023-05-17 16:47:29 +0900

    テストコードの実装

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -4,13 +4,13 @@ class OrderDestination
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :city
     validates :block
     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
     validates :user_id
     validates :item_id
   end
-  validates :sender_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :sender_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
---
+++ spec/factories/order_destinations.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_destination do
+    post_code { '123-4567' }
+    sender_id { 2 }
+    city { '市区町村' }
+    block { '1-1' }
+    building { '建物名1' }
+    phone_number { 12345678910 }
+
+  end
+end
